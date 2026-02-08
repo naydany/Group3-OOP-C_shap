313c3b6cdf892c565c784cbcc7c18a8a8a51faa5
EabPuth <eabputh0@gmail.com>
2026-02-08 12:34:27 +0700

    Car_update

---
+++ Reaching Car/Coin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Coin : GameObject
+{
+    public Coin(PictureBox picture, int initialSpeed) : base(picture, initialSpeed) { }
+
+    public override void Move()
+    {
+        if (Picture.Top >= 700)
+            Respawn();
+        else
+            base.Move();
+    }
+
+    public override void Respawn()
+    {
+        try
+        {
+            Picture.Top = -100;
+            Picture.Left = GameManager.GetSafeCoinX(Picture);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Coin respawn error: " + ex.Message);
+        }
+    }
+}
